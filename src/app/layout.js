f2b71084502ceb89bb5d305d3f08108f404ce31e
judilegend <judilegend2@gmail.com>
2024-04-20 21:55:30 +0300

    push hero

---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import "./globals.css";
+
+
+export const metadata = {
+  title: "Home Page",
+  description: "Portfolio",
+};
+import { Providers } from "./providers";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" >
+      <body className="h-screen">
+      
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Image from "next/image";
+import "./globals.css";
+import Hero from './components/Hero'
+export const metadata = {
+  title: "Home Page",
+  description: "Portfolio",
+};
+
+export default function Home() {
+  return (
+    <main className="h-screen px-20 pt-4">
+        <Navbar/>
+        <Hero/>
+    </main>
+  );
+}
