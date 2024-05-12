4226dbff95cdc4f1b9c34018974442be5707b4e3
judilegend <judilegend2@gmail.com>
2024-05-12 17:14:23 +0200

    style card

--- src/app/components/Projects/Card.jsx
+++ src/app/components/Projects/Card.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import "./Card.css";
 const Card = (props) => {
   return (
-    <article class="card_project">
-      <div class="temporary_text">
-        <img src={props.capture} alt="photo" className="rounded-lg"/>
+    <article className="card_project">
+      <div className="temporary_text">
+        <img src={props.capture} alt="photo" className="rounded-lg" />
       </div>
-      <div class="card_content">
-        <span class="card_title">This is a Title</span>
-        <span class="card_subtitle">
-          Thsi is a subtitle of this page. Let us see how it looks on the Web.
-        </span>
-        <p class="card_description">
-          Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-          dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-          repudiandae aspernatur? Labore minus soluta consequatur placeat.
-        </p>
+      <div className="card_content">
+        <span className="card_title ">{props.tittle}</span>
+        <span className="card_subtitle h-10">{props.sub_title}</span>
+        <div className="card_description flex gap-2 flex-wrap items-center h-[100px]">
+          <p>{props.descri}</p>
+          <span>
+            <img src={props.desc} className="w-8 h-8" />
+          </span>
+          <span>
+            <img src={props.desc1} className="w-8 h-8" />
+          </span>
+          <span>
+            <img src={props.desc2} className="w-8 h-8" />
+          </span>
+          <span>
+            <img src={props.desc3} className="w-8 h-8" />
+          </span>
+          <a href="">
+            <img src="/github.svg" className="w-8 h-8" />
+          </a>
+        </div>
       </div>
     </article>
   );
--- src/app/components/Projects/Projects.jsx
+++ src/app/components/Projects/Projects.jsx
@@ -16,11 +16,47 @@ const Projects = () => {
         <SimpleTittle name="My last Project" />
       </div>
       <div className="grid grid-cols-3 gap-10 justify-center items-center">
-        <Card capture="/assets/foodapp.png" />
-        <Card capture="/assets/gcoms.png" />
+        <Card
+          capture="/assets/ihm.png"
+          tittle="Edt-app"
+          sub_title="This project is a projet at school ,this is a gestion de sheldure a the ENI "
+          descri="This is made by NextJs for the frontEnd an NodeJS the backend"
+          desc3="/express.svg"
+          desc="/tailwind.svg"
+          desc2="/ts.svg"
+          desc1="/next.svg"
+        />
+        <Card
+          capture="/assets/foodapp.png"
+          tittle="Food-app"
+          sub_title="This project is a about a food application "
+          descri="This is a MERN app ,made by React and Express Js"
+          desc3="/express.svg"
+          desc="/css.svg"
+          desc2="/js.svg"
+          desc1="/react.svg"
+        />
+        <Card
+          capture="/assets/gcoms.png"
+          tittle="Gcoms"
+          sub_title="This project is a about a Commentary and interaction "
+          descri="This is a MERN app ,made by React and Express Js"
+          desc3="/express.svg"
+          desc="/css.svg"
+          desc2="/js.svg"
+          desc1="/react.svg"
+        />
 
-        <Card capture="/assets/foodapp1.png" />
-        <Card capture="/assets/foodapp.png" />
+        <Card
+          capture="/assets/ando.png"
+          tittle="Portfolio"
+          sub_title="This project is a about a food application "
+          descri="This is a MERN app ,made by React and Express Js"
+          desc3="/ts.svg"
+          desc="/tailwind.svg"
+          desc2="/js.svg"
+          desc1="/react.svg"
+        />
       </div>
     </div>
   );
