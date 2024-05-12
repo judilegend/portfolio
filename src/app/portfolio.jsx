aadd7349195c61015ce503f813f01744880bcdd9
judilegend <judilegend2@gmail.com>
2024-05-12 19:44:56 +0200

    add footer

--- src/app/portfolio.jsx
+++ src/app/portfolio.jsx
@@ -13,24 +13,29 @@ export const metadata = {
 };
 export default function Home() {
   return (
-    <main className=" max-w-[1250px] mx-auto pt-4 relative">
-      <Navbar />
-      <Hero />
-      <div className="flex items-center justify-center">
-        <About />
-      </div>
-      <div className="fixed left-8 bottom-10">
-        <ICons />
-      </div>
-      <div className="flex items-center justify-center">
-        <Skills />
-      </div>
-      <div className="flex items-center justify-center">
-        <Projects />
-      </div>
-      <div className="flex items-center h-[100vh]">
-        <Contact />
-      </div>
-    </main>
+    <>
+      <main className=" max-w-[1250px] mx-auto pt-4 relative">
+        <Navbar />
+        <Hero />
+        <div className="flex items-center justify-center">
+          <About />
+        </div>
+        <div className="fixed left-8 bottom-10">
+          <ICons />
+        </div>
+        <div className="flex items-center justify-center">
+          <Skills />
+        </div>
+        <div className="flex items-center justify-center">
+          <Projects />
+        </div>
+        <div className="flex items-center h-[100vh]">
+          <Contact />
+        </div>
+      </main>
+      <footer className="w-full h-12 bg-blue-500 flex items-center items-center  ">
+        <h1 className="w-[300px] mx-auto">Judi-Legend hightlight 2024</h1>
+      </footer>
+    </>
   );
 }
