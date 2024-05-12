0160ac2be912b47b89734fb0ef7ce4f93bbdaf6c
judilegend <judilegend2@gmail.com>
2024-05-12 19:27:55 +0200

    style contact

--- src/app/components/Contact/Contact.jsx
+++ src/app/components/Contact/Contact.jsx
@@ -4,12 +4,15 @@ import Formulaire from "./Formulaire";
 const Contact = () => {
   return (
     <div className="h-[105vh] flex px-4 items-center">
-      <main>
+      <main className="flex items-center justify-center">
         <section>
           <SimpleTittle name="Contact me" />
           <div className="mt-20 px-8">
             <Formulaire />
           </div>
+        </section>
+        <section>
+          <img src="/Sans titre-2.png" alt="" className="w-[500px]" />
         </section>
       </main>
     </div>
