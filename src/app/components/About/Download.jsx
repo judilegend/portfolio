dcce6f3d68007a5f60845a5d48b429bdefd657ad
judilegend <judilegend2@gmail.com>
2024-05-24 06:09:44 +0200

    changes-about

--- src/app/components/About/Download.jsx
+++ src/app/components/About/Download.jsx
@@ -5,26 +5,28 @@ const Download = () => {
     <div class=" duration-500 [transform:rotate3d(20_,-10,_1,_60deg)]  hover:-rotate-0 hover:mt-5 group border border-sky-900   overflow-hidden rounded-2xl relative h-[200px] w-[400px] bg-sky-800 p-5 flex flex-col items-start gap-4">
       <div class="text-gray-50">
         <span class="font-bold text-5xl">Jr</span>
-        <p class="text-xs">Frontend Developer</p>
+        <p class="text-xs">Frontend </p>
       </div>
-      <button class="duration-300 hover:bg-sky-900 border hover:text-gray-50 bg-gray-50 font-semibold text-sky-800 px-3 py-2 flex flex-row items-center gap-3">
-        Dowload CV
-        <svg
-          y="0"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0"
-          width="100"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="xMidYMid meet"
-          height="100"
-          class="w-6 h-6 fill-current"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M22.1,77.9a4,4,0,0,1,4-4H73.9a4,4,0,0,1,0,8H26.1A4,4,0,0,1,22.1,77.9ZM35.2,47.2a4,4,0,0,1,5.7,0L46,52.3V22.1a4,4,0,1,1,8,0V52.3l5.1-5.1a4,4,0,0,1,5.7,0,4,4,0,0,1,0,5.6l-12,12a3.9,3.9,0,0,1-5.6,0l-12-12A4,4,0,0,1,35.2,47.2Z"
-          ></path>
-        </svg>
-      </button>
+      <a href="/cv-albertinot.pdf">
+        {/* <button class="duration-300 hover:bg-sky-900 border hover:text-gray-50 bg-gray-50 font-semibold text-sky-800 px-3 py-2 flex flex-row items-center gap-3">
+          Dowload CV
+          <svg
+            y="0"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0"
+            width="100"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid meet"
+            height="100"
+            class="w-6 h-6 fill-current"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M22.1,77.9a4,4,0,0,1,4-4H73.9a4,4,0,0,1,0,8H26.1A4,4,0,0,1,22.1,77.9ZM35.2,47.2a4,4,0,0,1,5.7,0L46,52.3V22.1a4,4,0,1,1,8,0V52.3l5.1-5.1a4,4,0,0,1,5.7,0,4,4,0,0,1,0,5.6l-12,12a3.9,3.9,0,0,1-5.6,0l-12-12A4,4,0,0,1,35.2,47.2Z"
+            ></path>
+          </svg>
+        </button> */}
+      </a>
 
       <svg
         viewBox="0 0 64 64"
